9e54bbb73ef626169fa9f21439029e8a60bea883
panliang <panliang@17shanyuan.com>
2021-03-24 14:48:18 +0800

    Initial commit

---
+++ Example/PLCategoryPubKit/BBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BBAppDelegate.h
+//  PLCategoryPubKit
+//
+//  Created by solerpan on 03/24/2021.
+//  Copyright (c) 2021 solerpan. All rights reserved.
+//
+
+@import UIKit;
+
+@interface BBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/PLCategoryPubKit/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  PLCategoryPubKit
+//
+//  Created by solerpan on 03/24/2021.
+//  Copyright (c) 2021 solerpan. All rights reserved.
+//
+
+@import UIKit;
+#import "BBAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BBAppDelegate class]));
+    }
+}
